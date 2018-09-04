b5e016e05b36f6d3eb82dd5d296efd3476eed4a0
Ji-Soo <Ji-Soo@DESKTOP-HSA9UHR.fritz.box>
2018-09-04 17:45:27 +0200

    Initial commit.

---
+++ RESTService/src/main/java/edivelog/EDiveLog/EDiveLogApplication.java
@@ -0,0 +1,27 @@
+/*
+ * Author: Amon Druffel
+ * Date: 04.09.2018
+ * 
+ * File: EDiveLogApplication
+ * 
+ */
+
+
+package edivelog.EDiveLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <b>Description:</b><br>
+ * 
+ * Starting class required to start Spring application using spring boot
+ *
+ */
+@SpringBootApplication
+public class EDiveLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EDiveLogApplication.class, args);
+	}
+}
