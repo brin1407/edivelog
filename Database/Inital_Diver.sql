b592f3b822d035de34b18cddf79aa8ec49f79f19
brin1407 <rumpel_michael@web.de>
2018-12-24 16:37:45 +0100

    delete initial data queries (database update && structure update)

--- Database/Inital_Diver.sql
+++
@@ -1,7 +0,0 @@
--- INSERT TEST DATA
-
-INSERT INTO 
-`Diver` (`Lastname`, `Firstname`, `Username`, `Birth`, `Street`, `Housenumber`, `Postcode`, `Location`,
-           `Country`, `AcceptTermOfUse`, `AcceptTermOfUseDate`, `AcceptPrivacy`,`AcceptPrivacyDate`)
-VALUES
-('Mustermann', 'Max', 'max.mustermann@mustermann.de', '2000-01-01', 'Musterstraße', '1', '12345', 'Musterstadt', 9, TRUE,  NOW(), TRUE,  NOW());
